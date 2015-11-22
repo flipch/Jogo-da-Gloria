5abb97397c36f2f878f70d9dd8e12aa7003c4510
felipe <felip@DESKTOP-6N3GDRS>
2015-11-22 19:34:52 +0000

    First commit

---
+++ Jogo da Glória/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace Jogo_da_Glória
+{
+    /// <summary>
+    /// First page where you select how many players are playing.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private void bt_go_Click(object sender, RoutedEventArgs e)
+        {
+            if (list_players.SelectedValue != null) // If something is selected then allow to navigate
+                this.Frame.Navigate(typeof(Game));
+        }
+
+        private void list_players_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                list_players.Items.Add(i);
+            }
+
+            //Sending the player number to Game
+            var obj = App.Current as App;
+            obj.players = (int)list_players.SelectedValue;
+        }
+    }
+}
